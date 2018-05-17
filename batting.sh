0fd7e3236417270393178ea13917e6baab56104b
Trevor Dawe <trevor.dawe@gmail.com>
2018-05-16 21:17:23 -0300

    Added speed calculation

--- batting.sh
+++ batting.sh
@@ -12,6 +12,13 @@ scalingSpeed=`bc <<< "scale=4; 100/$maxSpeed"`
 echo "Max Power from $1 is $maxPower and scaling is $scalingPower"
 echo "Max Speed from $1 is $maxSpeed and scaling is $scalingSpeed"
 
-sqlite3 -header -column database.db "select nameFirst, nameLast, cast(((SLG-AVG)*1000)/3*$scalingPower as integer) as Power, cast($speedCalculation*$scalingSpeed as integer) as Speed from Batting left join People on "Batting".playerId = "People".playerId where teamId='$2' and yearId='$1' and AB > 0 order by Power asc;"
+load_extension="select load_extension(\"./libsqlitefunctions.so\");"
+f1_speed="(20.0 * (((SB + 3.0)/(SB + CS + 7.0)) - 0.4))"
+f2_speed="((1/0.07) * sqrt((SB + CS + 0.0)/(\"1B\" + BB + HBP + 0.0)))"
+f3_speed="(625 * ((\"3B\") / (AB + HR + SO + 0.0)))"
+f4_speed="(25.0 * (((R-HR+0.0)/(H+BB+HBP-HR+0.0)) - 0.1))"
+f5_speed="((1/0.007) * (0.063 - (GIDP/(AB-HR-SO+0.0))))"
+#f6_speed="TODO"
+final_speed_calculation="($f1_speed + $f2_speed + $f3_speed + $f4_speed + $f5_speed) / 5"
 
-#f1_speed="(20 * (((SB + 3)/(SB + CS + 7)) - 0.4))"
+sqlite3 -header -column database.db "$load_extension select nameFirst, nameLast, cast(((SLG-AVG)*1000)/3*$scalingPower as integer) as Power, round($final_speed_calculation * 10) as Speed from Batting left join People on "Batting".playerId = "People".playerId where teamId='$2' and yearId='$1' and AB > 100 order by Speed asc;"
