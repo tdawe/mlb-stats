08b0f8149300de579c6c4065c037f6193b3870fe
Trevor Dawe <trevor.dawe@gmail.com>
2018-05-29 21:30:30 -0300

    Removed old speed calculation

--- batting.sh
+++ batting.sh
@@ -5,12 +5,7 @@ minGames=200
 maxPower=`sqlite3 database.db "select max(((SLG-AVG)*1000)/3) from Batting where yearId='$1' and AB > $minGames;"`
 scalingPower=`bc <<< "scale=4; 100/$maxPower"`
 
-speedCalculation="(\"3B\" + \"SB\"*2)"
-maxSpeed=`sqlite3 database.db "select max($speedCalculation) from Batting where yearId='$1' and AB > $minGames;"`
-scalingSpeed=`bc <<< "scale=4; 100/$maxSpeed"`
-
 echo "Max Power from $1 is $maxPower and scaling is $scalingPower"
-echo "Max Speed from $1 is $maxSpeed and scaling is $scalingSpeed"
 
 load_extension="select load_extension(\"./libsqlitefunctions\");"
 f1_speed="(20.0 * (((SB + 3.0)/(SB + CS + 7.0)) - 0.4))"
